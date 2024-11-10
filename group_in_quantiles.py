bc348d8151ddb8d42ffeaf5e7e8a90fd63ab44ea
Manoj Kumar <90147485+manojmahan@users.noreply.github.com>
2024-11-10 20:08:49 +0530

    Update group_in_quantiles.py

--- group_in_quantiles.py
+++ group_in_quantiles.py
@@ -8,9 +8,10 @@ num_quantiles = int(sys.argv[1])
 
 quantiles, bins = pd.qcut(numbers, num_quantiles, labels=[f'q{i+1}' for i in range(num_quantiles)], retbins=True)
 
-
+#print(quantiles,bins)
+#print(quantiles.categories)
 for number, quantile in zip(numbers, quantiles):
-    # Find the correct bin interval
+    # bin interval
     bin_start = bins[quantiles.categories.get_loc(quantile)]
     bin_end = bins[quantiles.categories.get_loc(quantile) + 1]
     print(f'{number}\t{quantile}\t{quantile} ({bin_start}, {bin_end}]')
