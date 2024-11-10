82a7934aef5a5f992a2d291d4db5ba1b77e155e0
Manoj Kumar <90147485+manojmahan@users.noreply.github.com>
2024-11-10 20:14:57 +0530

    Update plot.py

--- plot.py
+++ plot.py
@@ -6,7 +6,7 @@ import sys
 data = pd.read_csv(sys.argv[1], sep="\t")
 
 # Create the heatmap
-plt.figure(figsize=(2,5))  # Adjust figure size as needed
+plt.figure(figsize=(4,8))  # Adjust figure size as needed
 # Define custom colormap
 cmap = LinearSegmentedColormap.from_list(
     "custom_cmap", [(0, "white"), (0.25, "red"), (0.5, "grey"), (0.75, "yellow"), (1, "green")]
