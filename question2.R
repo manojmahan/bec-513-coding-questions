3e81861038c392f394d0ad692b848710545a7aeb
Manoj Kumar <90147485+manojmahan@users.noreply.github.com>
2024-11-10 20:05:49 +0530

    Update question2.R

--- question2.R
+++ question2.R
@@ -1,13 +1,13 @@
 library(ggplot2)
 
 data <- read.table(file("stdin"), header = FALSE, sep = "\t", col.names = c("X", "Y", "Category"))
-
+# print(head(data))
 args <- commandArgs(trailingOnly = TRUE)
 output_file <- args[1]            
 x_label <- args[2]                 
 y_label <- args[3]                 
 plot_title <- args[4]             
-
+#print(output_file)
 plot <- ggplot(data, aes(x = X, y = Y, color = Category)) +
   geom_line() +
   labs(title = plot_title, x = x_label, y = y_label)
